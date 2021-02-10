d30b3e2be33028243a1f01789912918890f70485
David Schreur <13521116+davidskur@users.noreply.github.com>
2021-02-10 11:46:57 -0500

    Updates

--- FileReadifierTurbo.h
+++ FileReadifierTurbo.h
@@ -1,22 +1,59 @@
 #ifndef _FILEREADIFIERTURBO
 #define _FILEREADIFIERTURBO
 
+#include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 class FileReadifierTurbo {
+
+private:
+    fstream wordFile;
 	
 public:
 	// constructor
 	FileReadifierTurbo() {};
 	// destructor
-	~FileReadifierTurbo() {};
+	~FileReadifierTurbo() { wordFile.close(); } // TODO: Read more about destructors. This pry isn't needed.
 
-	string GetWordList(string path) {
+    // current dir, included txt file
+    vector<string> GetWordList() {
+        vector<string> words;
 
-	}
-	string GetFileText(string path) {
-
+        wordFile.open("words.txt", fstream::in);
+        if (!wordFile) {
+            cout << "\nFile not found!" << endl;
+            return { }; // list initialization
+        }
+        else if (wordFile.is_open()) { // file found
+            string newLine;
+            while (getline(wordFile, newLine)) {
+                words.push_back(newLine);
+            }
+        }
+        wordFile.close();
+        return words;
+    }
+    // overload
+	vector<string> GetWordList(string fileName) {
+        vector<string> words;
+        
+        wordFile.open(fileName, fstream::in);
+        if (!wordFile) {
+            cout << "\nFile not found!" << endl;
+            return { };
+        }
+        else if (wordFile.is_open()) {
+            string newLine;
+            while (getline(wordFile, newLine)) {
+                words.push_back(newLine);
+            }
+        }
+        wordFile.close();
+        return words;
 	}
 };
-
 #endif
